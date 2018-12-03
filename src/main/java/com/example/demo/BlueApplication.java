22578a6e492a89b3ac229b0f30286c9b11acf015
arst8 <arst8@192.168.2.177>
2018-12-03 12:39:13 +0100

    First commit of the project

---
+++ src/main/java/com/example/demo/BlueApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueApplication.class, args);
+	}
+}
